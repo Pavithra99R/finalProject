2ff0ca09f25bfe1ad4c2ec69dcd0dbf60a0ebbc6
Geethanjali Thati <geethanjali.thati@lntinfotech.com>
2021-01-09 13:35:39 +0530

    Adding Booking and Passenger Entities

---
+++ project/src/main/java/com/lti/entity/Booking.java
@@ -0,0 +1,102 @@
+package com.lti.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "booking")
+public class Booking {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "ticket_id")
+	private int ticketId;
+
+	private String boarding;
+	private String droping;
+	private double amount;
+
+	@Column(name = "paid_date")
+	private LocalDate paidDate;
+	private String status;
+
+	@ManyToOne
+	@JoinColumn(name = "route_id")
+	private Route route;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	public int getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(int ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public String getBoarding() {
+		return boarding;
+	}
+
+	public void setBoarding(String boarding) {
+		this.boarding = boarding;
+	}
+
+	public String getDroping() {
+		return droping;
+	}
+
+	public void setDroping(String droping) {
+		this.droping = droping;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getPaidDate() {
+		return paidDate;
+	}
+
+	public void setPaidDate(LocalDate paidDate) {
+		this.paidDate = paidDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Route getRoute() {
+		return route;
+	}
+
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+
+	public Passenger getUser() {
+		return user;
+	}
+
+	public void setUser(Passenger user) {
+		this.user = user;
+	}
+
+}
---
+++ project/src/main/java/com/lti/entity/Passenger.java
@@ -0,0 +1,70 @@
+package com.lti.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "passenger")
+public class Passenger {
+
+	private int passengerId;
+	private String passengerName;
+	private int passengerAge;
+	private String passengerGender;
+	private int seatNo;
+
+	@ManyToOne
+	@JoinColumn(name = "ticket_id")
+	private Booking booking;
+
+	public int getPassengerId() {
+		return passengerId;
+	}
+
+	public void setPassengerId(int passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+
+	public int getPassengerAge() {
+		return passengerAge;
+	}
+
+	public void setPassengerAge(int passengerAge) {
+		this.passengerAge = passengerAge;
+	}
+
+	public String getPassengerGender() {
+		return passengerGender;
+	}
+
+	public void setPassengerGender(String passengerGender) {
+		this.passengerGender = passengerGender;
+	}
+
+	public int getSeatNo() {
+		return seatNo;
+	}
+
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+
+	public Booking getBooking() {
+		return booking;
+	}
+
+	public void setBooking(Booking booking) {
+		this.booking = booking;
+	}
+
+}
