637ad45c1148b54389abc581c57af8c9bd0257cd
Pavithra Ravi <PAVITHRA3.R@lntinfotech.com>
2021-01-10 19:16:56 +0530

    committed

---
+++ project/src/test/java/com/lti/test/RouteBusTest.java
@@ -0,0 +1,26 @@
+package com.lti.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.lti.dao.RouteBus;
+
+public class RouteBusTest {
+	
+	@Test
+	public void getBusForRoute() {
+		
+		RouteBus dao = new RouteBus();
+		List<Object[]> buses = dao.fetchBusForRoute("Chennai", "Kerala");
+		for(Object[] b:buses) {
+			System.out.println(b[0]);
+			System.out.println(b[1]);
+			System.out.println(b[2]);
+		}
+		
+	}
+
+}
