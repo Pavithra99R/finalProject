d6b335174e88dabdb61ff415b86210a78f8c35b6
Pavithra Ravi <PAVITHRA3.R@lntinfotech.com>
2021-01-10 21:19:09 +0530

    Added Repository and controller package

---
+++ project/src/main/java/com/lti/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.lti.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "It is working";
+	}
+
+}
